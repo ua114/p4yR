10593d4544201e4505b98eda30603fb7eebaa890
Usman Ammar <usmanammar@MacBook-Pro-4.local>
2021-09-15 22:43:54 +0100

    full file

--- week1.py
+++ week1.py
@@ -54,6 +54,29 @@
 
 # == tests whether objects have the same value, whereas is tests whether...
 # ...objects have the same identity.
+# Part 2..........
+
+# Lists
+
+# numbers = [1, 2, 3, 4, 5]
+#
+# print(numbers[1])
+# print(numbers[-0])
+# # - index start from the end of the list (i.e. the right)
+# print(numbers[-1])
+# print(numbers[-2])
+#
+# numbers2 = [10, 9, 8, 7, 6]
+# # concatenate lists using the '+' operator
+# all_numbers = numbers + numbers2
+#
+# # sort reorders the current list (list method)
+# all_numbers.sort()
+# print(all_numbers)
+#
+# # sorted creates a new list
+# num3 = sorted(all_numbers, reverse=True)
+# print(num3)
 
 # Tuples.........
 # They are immutable sequences of objects
