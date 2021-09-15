3d172fcd08baddb618adc2f7b776d84c13a20cd9
Usman Ammar <usmanammar@MacBook-Pro-4.local>
2021-09-15 22:43:08 +0100

    full file

--- week1.py
+++ week1.py
@@ -1,23 +1,59 @@
-# Part 2..........
+# Basic Arithematic Operations
 
-# Lists
+# x = 123 + 64
+# print(x)
+#
+# y = 123 * 64
+# print(y)
+#
+# z = 123 ** 64
+# print(z)
+#
+# a = 123 / 64
+# print(a)
+#
+# b = 123 // 64 # Integer division
+# print(b)
+#
+# # Extra commands that use the math module
+# import math
+#
+# c = math.factorial(5)
+# print(c)
 
-numbers = [1, 2, 3, 4, 5]
+# Using random module
 
-print(numbers[1])
-print(numbers[-0])
-# - index start from the end of the list (i.e. the right)
-print(numbers[-1])
-print(numbers[-2])
+# import random
+#
+# d = random.choice([2,44,55,66]) #Picks a random number from the list
+# print(d)
+#
+# e = random.choice(['apple','orange','banana'])
+# print(e)
 
-numbers2 = [10, 9, 8, 7, 6]
-# concatenate lists using the '+' operator
-all_numbers = numbers + numbers2
+# Boolean operators
 
-# sort reorders the current list (list method)
-all_numbers.sort()
-print(all_numbers)
+# print(type(True))
+#
+# print(True or False)
+# print(False or False)
+# print(True and False)
+# print(not True)
+#
+# a = [1,2,4]
+# b = [1,2,4]
+# c = [3,5,6]
+#
+# # Comparative statmenets return True or False
+# print(a == b)
+# print(a != b)
+# print(a == c)
+# print(c > a)
+#
+# print(a is b) # False as a and b contain the same but are diff objects
 
-# sorted creates a new list
-num3 = sorted(all_numbers, reverse=True)
-print(num3)
+# == tests whether objects have the same value, whereas is tests whether...
+# ...objects have the same identity.
+
+# Tuples.........
+# They are immutable sequences of objects
