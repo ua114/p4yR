e1ea0bab4acd83513ffacdb2b65bed1360db5d78
Usman Ammar <usmanammar@MacBook-Pro-4.home>
2021-09-22 23:34:11 +0100

    done for the night

---
+++ week2part2.py
@@ -0,0 +1,28 @@
+# ...........................................................................
+# Numpy arrays
+# ...........................................................................
+
+# Making vectors and matrices
+import numpy as np
+
+# # Zero vectors
+# zero_vector = np.zeros(3)
+# print(zero_vector)
+#
+# zero_matrix = np.zeros( (3,4) )
+# print(zero_matrix)
+#
+# # Identity matrices
+# identity_matrix = np.ones( (3,3) )
+# print(identity_matrix)
+
+# Arrays
+
+# x = np.array([1,2,3])
+# print(x)
+
+y = np.array([ [1,2],[3,4] ])
+print(y)
+
+# Transpose function
+print(y.transpose())
