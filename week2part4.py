f63104f47cf01bf9a7c5bf1a8f88dedd4da60640
Usman Ammar <usmanammar@MacBook-Pro-4.local>
2022-01-02 00:59:43 +0000

    almost done

--- week2part4.py
+++ week2part4.py
@@ -119,7 +119,7 @@ import time
 
 # Displacements in x and y
 # X_0 = np.array([[0],[0]]) #starting position (0,0)
-# delta_X = np.random.normal(0,1,(2,5000000))
+# delta_X = np.random.normal(0,1,(2,50))
 # X = np.concatenate((X_0, np.cumsum(delta_X, axis=1)), axis =1)
 #
 # plt.plot(X[0], X[1], "yo-",markersize=0.01)
